a0ca6b84223181d151a2127476ce2d40751de5c4
iaceene <yassineajagrou986@gmail.com>
2025-01-15 17:10:48 +0100

    finish norm

--- mandatory/src/graphic/render_utiles.c
+++ mandatory/src/graphic/render_utiles.c
@@ -6,35 +6,72 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:56:53 by yaajagro          #+#    #+#             */
-/*   Updated: 2025/01/15 16:59:58 by yaajagro         ###   ########.fr       */
+/*   Updated: 2025/01/15 17:10:20 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-void    ft_map_rebuild(t_vars *vars, int move)
+void	ft_up(t_vars *vars)
 {
-	int x = vars->data->player_x;
-	int y = vars->data->player_y;
+	int	x;
+	int	y;
 
+	x = vars->data->player_x;
+	y = vars->data->player_y;
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->worm, x * 64, y * 64);
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->ground, x * 64, (y + 1) * 64);
+}
+
+void	ft_down(t_vars *vars)
+{
+	int	x;
+	int	y;
+
+	x = vars->data->player_x;
+	y = vars->data->player_y;
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->worm, x * 64, y * 64);
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->ground, x * 64, (y - 1) * 64);
+}
+
+void	ft_left(t_vars *vars)
+{
+	int	x;
+	int	y;
+
+	x = vars->data->player_x;
+	y = vars->data->player_y;
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->worm, x * 64, y * 64);
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->ground, (x + 1) * 64, y * 64);
+}
+
+void	ft_right(t_vars *vars)
+{
+	int	x;
+	int	y;
+
+	x = vars->data->player_x;
+	y = vars->data->player_y;
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->worm, x * 64, y * 64);
+	mlx_put_image_to_window(vars->mlx, vars->win,
+		vars->img->ground, (x - 1) * 64, y * 64);
+}
+
+void	ft_map_rebuild(t_vars *vars, int move)
+{
 	if (move == 1)
-	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->worm,  x * 64, y * 64);
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->ground,  x * 64, (y + 1) * 64);
-	}
+		ft_up(vars);
 	if (move == 2)
-	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->worm,  x * 64, y * 64);
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->ground,  x * 64, (y - 1) * 64);
-	}
+		ft_down(vars);
 	if (move == 3)
-	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->worm,  x * 64, y * 64);
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->ground,  (x + 1) * 64, y * 64);
-	}
+		ft_left(vars);
 	if (move == 4)
-	{
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->worm,  x * 64, y * 64);
-		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->ground,  (x - 1) * 64, y * 64);
-	}
+		ft_right(vars);
 }
