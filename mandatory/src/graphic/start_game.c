e51064dea577126cb26154b2fa9d7a89b747d16b
iaceene <yassineajagrou986@gmail.com>
2025-01-17 14:28:49 +0100

    fix bugs

--- mandatory/src/graphic/start_game.c
+++ mandatory/src/graphic/start_game.c
@@ -6,7 +6,7 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 20:33:46 by yaajagro          #+#    #+#             */
-/*   Updated: 2025/01/15 18:12:30 by yaajagro         ###   ########.fr       */
+/*   Updated: 2025/01/17 14:27:25 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,14 @@ void	start_game(t_list *data)
 		return ;
 	else
 		vars.mlx = mlx_init();
+	if (!vars.mlx)
+		return ;
 	size_x = (data->wight - 1) * 64;
 	size_y = data->hight * 64;
 	vars.data = data;
 	vars.win = mlx_new_window(vars.mlx, size_x, size_y, "Worm");
+	if (!vars.win)
+		return ;
 	ft_render(&vars, 0);
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_loop(vars.mlx);
